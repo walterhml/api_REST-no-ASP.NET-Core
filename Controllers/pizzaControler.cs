7a66234699e11952d1d3b377340d0c2ce0a96f29
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-03-10 17:04:18 -0300

    Add files via upload

---
+++ Controllers/pizzaControler.cs
@@ -0,0 +1,24 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+
+    // GET by Id action
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+}
